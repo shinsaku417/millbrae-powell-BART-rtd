602fbda1a59defd271937b3fb6fba50a3053b719
Shin Uesugi <shinsaku417@gmail.com>
2015-01-09 10:18:35 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Millbrae-Powell BART RTD
+
+Simple and customizable chrome extension that allows you to see real time
+departures for BART trains that go from Millbrae to Powell Street or vice versa
+while browsing on Google Chrome.
+
+![Demo](https://s3.amazonaws.com/f.cl.ly/items/2g1F2q150d2H0h2G173t/Screen%20Recording%202015-01-09%20at%2010.11%20AM.gif)
+
+## Step by Step Customization
+
+1. Go to ![BART API Registration](http://api.bart.gov/api/register.aspx) and obtain an API key.
+2. Fork and Clone this repo or download zip.
+3. Open app.js in the file. In the url section of ajax, change the following:
+  * Specify the station by changing orig=mlbr. You can see full list of
+  station abbreviations ![here](http://api.bart.gov/docs/overview/abbrev.aspx).
+  * Insert your API key in key=insert-key-here.
+4. In the getEtd function, change the first if statement to specify
+line colors that you want. 'color' variable can be 'RED', 'BLUE', 'GREEN',
+'YELLOW', or 'ORANGE'.
+5. Open Google Chrome and enter chrome://extensions on the address bar.
+6. Check the Developer mode on top-right, and click Load unpacked extension.
+7. Select folder that you cloned or downloaded, and you're ready to use it!
---
+++ app.js
@@ -0,0 +1,76 @@
+$(function() {
+  // Get Real Time Departures for northbound trains from Millbrae
+  $.ajax ({
+    // Register for BART API at http://api.bart.gov/api/register.aspx and insert key
+    // Change orig=mlbr to station of your choice. Full list of abbreviations
+    // can be found at http://api.bart.gov/docs/overview/abbrev.aspx
+    url: "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=12th&dir=n&key=insert-key-here",
+    type: 'GET',
+    success: function (data) {
+      getEtd(data, '.northbound')
+    },
+    error: function (data) {
+      console.error('Error: Failed to fetch data');
+    }
+  });
+
+  // Get Real Time Departures for southbound trains from Powell Street
+  $.ajax ({
+    url: "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=powl&dir=s&key=insert-key-here",
+    type: 'GET',
+    success: function (data) {
+      getEtd(data, '.southbound');
+    },
+    error: function (data) {
+      console.error('Error:Failed to fetch data');
+    }
+  });
+
+  var getEtd = function(data, direction) {
+    var timeData = {
+      'redLine': [],
+      'blueLine': [],
+      'greenLine': [],
+      'yellowLine': [],
+      'orangeLine': []
+    };
+    $(data).find('estimate').each(function() {
+      var color = $(this).find('color')[0].innerHTML;
+      // Specify lines of your choice here
+      if (color === 'RED' || color === 'YELLOW') {
+        var min = $(this).find('minutes')[0].innerHTML;
+        switch(color) {
+          case 'RED':
+            timeData.redLine.push(min);
+            break;
+          case 'BLUE':
+            timeData.blueLine.push(min);
+            break;
+          case 'GREEN':
+            timeData.greenLine.push(min);
+            break;
+          case 'YELLOW':
+            timeData.yellowLine.push(min);
+            break;
+          case 'ORANGE':
+            timeData.orangeLine.push(min);
+            break;
+        }
+      }
+    });
+    appendData(timeData, direction);
+  };
+
+  var appendData = function(data, direction) {
+    for (var key in data) {
+      for (var i = 0; i < data[key].length; i++) {
+        var min = data[key][i];
+        if (min === "Leaving") {
+          $(direction).append('<div class = ' + key + '>Leaving</div>');
+        } else {
+          $(direction).append('<div class = ' + key + '><span class = "min">' + min + '</span> mins</div>');
+        }
+      }
+    }
+  }
+});
